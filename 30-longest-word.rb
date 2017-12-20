575daffe32a0e7b5ce42cd3f4d6a6dbd4bef93f5
bingo8670 <307286130@qq.com>
2017-12-21 00:19:51 +0800

    add find-short by sample method

--- 30-longest-word.rb
+++ 30-longest-word.rb
@@ -1,9 +1,17 @@
 print " 请输入英文句子，输完之后按 Enter:"
-words = gets.to_str
+s = gets.to_str
 
-def longest_word(words)
-  words.split(" ").sort! {|a_word, b_word| b_word.length <=>           a_word.length}[0]
+=begin 第一种方法
+def longest_word(s)
+  s.split(" ").sort! {|a_word, b_word| b_word.length <=>           a_word.length}[0]
+end
+=end
+
+#第二种方法
+def find_long(s)
+    # your code here
+    l = s.split.max_by(&:length)
+    return l # l: length of the shortest word
 end
 
-
-puts "最长的单词是 #{longest_word(words)}"
+puts "最长的单词是 #{find_long(s)}"
---
+++ 31-shortest-word.rb
@@ -0,0 +1,11 @@
+print " 请输入英文句子，输完之后按 Enter:"
+s = gets.to_str
+
+def find_short(s)
+    # your code here
+    l = s.split.min_by(&:length).size
+    return l # l: length of the shortest word
+end
+
+
+puts "最短的单词字母数是 #{find_short(s)}"
