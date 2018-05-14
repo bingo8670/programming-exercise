73b47c13dcd91e6833df5f30cf2ee8a31337e8de
bingo8670 <bingo891111@gmail.com>
2018-05-14 17:37:47 +0800

    nginx log method done

--- nginx_access_scan2.rb
+++ nginx_access_scan2.rb
@@ -1,71 +1,72 @@
-# 请输入 ruby nginx_access_scan2.rb nginx.access.log
+# 请输入 ruby nginx_access_scan2.rb
 
 require 'mysql2'
 require 'digest/md5'
 require 'rails'
 
-# client = Mysql2::Client.new(
-#     :host     => '127.0.0.1', # 主机
-#     :username => 'root',      # 用户名
-#     :password => '123123',    # 密码
-#     :database => 'weblog',    # 数据库
-#     :encoding => 'utf8'       # 编码
-#     )
-
-
-lines   = []
-ip      = []
-time    = []
-actor   = []
-url     = []
-url_md5 = []
-md5     = []
-state   = []
-length  = []
-refer   = []
-agent   = []
-datas   = []
+md5 = []
 dataes  = []
 count   = 0
 
-def open_file()
-  filename = ARGV[0]
-  file = File.open(filename)
+def open_file(path)
+  File.open(path)
 end
-# filename = ARGV[0]
-# file = File.open(filename)   # 文件目录
 
-open_file()
-# log以空行分段
+def time_format(time)
+  time = time[0].sub(/:/, ' ')
+  time.to_time.strftime("%Y-%m-%d %H:%M:%S")
+end
+
+def generate_md5(url)
+  url_md5 = Digest::MD5.hexdigest(url)
+end
+
+def url_unique(url, url_md5)
+  @md5 ||= []
+  # 如果是nil，那么让它成为一个空数组；如果它不是nil，那么什么也不做。
+  if @md5.include?(url_md5)
+    url = []
+  else
+    @md5       = @md5.push(url_md5)
+    return url
+  end
+end
+
+def insert_query(dataes)
+  client = Mysql2::Client.new(
+      :host     => '127.0.0.1', # 主机
+      :username => 'root',      # 用户名
+      :password => '123123',    # 密码
+      :database => 'weblog',    # 数据库
+      :encoding => 'utf8'       # 编码
+      )
+
+  dataes.each do |datas|
+    datas.each do |i|
+      client.query("INSERT INTO nginx(ip, time, actor, url, state, length, refer, agent) VALUES ('#{i[0]}', '#{i[1]}', '#{i[2]}', '#{i[3]}', '#{i[4]}', '#{i[5]}', '#{i[6]}', '#{i[7]}')")
+    end
+  end
+end
+
+file = open_file("./nginx.access.log")
+
 file.each_line do |line|
   count      += 1
   words       = line.split
   ip          = line.scan(/^\d{2}\.\d{2}\.\d{2}\.\d{2}/)
-
-# 处理time格式问题，先取消 2018:15 中间的“：”，然后使用 rails的to_time、strftime 方法
   time        = line.scan(/\d+\/\w+\/\d{4}\:\d{2}\:\d{2}\:\d{2}/)
-  time        = time[0].sub(/:/, ' ')
-  time        = [ time.to_time.strftime("%Y-%m-%d %H:%M:%S") ]
-
+  time        = [ time_format(time) ]
   actor       = [line.scan(/[A-Z]{3}/)[0]]
-
-# 要求URL唯一，如果重复只保留第一条；将 URL 转换为 md5 加密值，经过比对，如果重复，URL = []
-# Digest::MD5.hexdigest('abc')      #=> "90015098..."
   url         = [line.scan(/\w*\/\w*\/*\w*\/*\w*/)[1]]
-  url_md5     = [ Digest::MD5.hexdigest(url[0]) ]
-  if md5.include?(url_md5)
-    url = []
-  else
-    md5       = md5.push(url_md5)
-    url       = [line.scan(/\w*\/\w*\/*\w*\/*\w*/)[1]]
-  end
+  url_md5     = [ generate_md5(url[0]) ]
+  url = [ url_unique(url, url_md5) ]
 
   state       = [line.scan(/\d{3}/)[2]]
   length      = [words[9]]
   refer       = [words[10]]
   agent       = [words[-7..-2].join(" ")]
 
-  if url == []
+  if url == [[]]
     datas = []
   else
     datas     = [ ip + time + actor + url + state + length + refer + agent ]
@@ -76,17 +77,7 @@ file.each_line do |line|
 end
 
 p "共有#{count} 条日志记录，提取信息如上所示👆"    # 如上所示👆、如下所示👇
-# p dataes
-# 显示每行提取字段的合集，然后再合集，比较美观；
 
+insert_query(dataes)
 
-# dataes.each do |datas|
-#   datas.each do |i|
-#     client.query("INSERT INTO nginx(ip, time, actor, url, state, length, refer, agent) VALUES ('#{i[0]}', '#{i[1]}', '#{i[2]}', '#{i[3]}', '#{i[4]}', '#{i[5]}', '#{i[6]}', '#{i[7]}')")
-#   end
-# end
-
-# p "#{dataes[0][0][0]}"
-
-# 已验证单行数据可插入成功
-  #client.query(" INSERT INTO nginx(ip, time, actor, url, state, length, refer, agent) VALUES ('#{dataes[0][0][0]}', '#{dataes[0][0][1]}', '#{dataes[0][0][2]}', '#{dataes[0][0][3]}', '#{dataes[0][0][4]}', '#{dataes[0][0][5]}', '#{dataes[0][0][6]}', '#{dataes[0][0][7]}' ) ")
+file.close
