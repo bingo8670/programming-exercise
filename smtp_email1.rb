ee653d21e26dbbd121dacd812289cde40e863be6
bingo8670 <307286130@qq.com>
2018-05-19 23:27:10 +0800

    smtp method

---
+++ smtp_email1.rb
@@ -0,0 +1,22 @@
+# 加入Git忽视文件
+# SMTP(Simple Mail Transfer Protocol),即简单邮件传输协议,它是一组用于由源地址到目的地址传送邮件的规则，由它来控制信件的中转方式。
+
+require 'net/smtp'
+
+FROM_ADDRESS = "18535150344@163.com"
+
+def reply(to, subject, msg)
+  mail = ( "To: #{to}\r\n" +
+  "From: #{FROM_ADDRESS}\r\n" +
+  "Subject: #{subject}\r\n" +
+  "\r\n" +
+  msg )
+
+  Net::SMTP.start('smtp.163.com',
+                  25,
+                  'localhost',
+                  '1811111111', '******', :plain) do |smtp|
+  smtp.send_mail(mail, FROM_ADDRESS,
+  [ to, 'xxxx@qq.com' ])
+  end
+end
